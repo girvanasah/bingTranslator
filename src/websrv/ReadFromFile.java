7808c94bf3283cadc8d9f5ef7d51083699313230
girvanasah <girvanasah@live.com>
2016-07-06 09:58:26 +0530

    bing translator api and gui

--- src/websrv/ReadFromFile.java
+++ src/websrv/ReadFromFile.java
@@ -16,7 +16,6 @@ import org.apache.http.impl.client.HttpClientBuilder;
 public class ReadFromFile extends WebServices
 {	Connections con;
 	TokenObtain tokn;
-	
 	String token;
 	String finaltoken;
 	String from;
@@ -27,7 +26,8 @@ public class ReadFromFile extends WebServices
 	ArrayList<String> resultFromAPI=new ArrayList<>();
 	ArrayList<String> fileRead()
 	{	
-		try{ 
+		try
+		{ 
 			String csvFile = "/home/girvanasahchaurasia/Downloads/input.csv";
 			String csvFile1 = "/home/girvanasahchaurasia/Downloads/codes.csv";
 			BufferedReader br1=null;
@@ -42,85 +42,69 @@ public class ReadFromFile extends WebServices
 			HashMap<String, String> arr=new HashMap<>();
 			while ((line1 = br1.readLine()) != null)
 			{ 
-//				codeTuple cd = new codeTuple( null , null );
 				inputValues1 = line1.split(cvsSplitBy);
 				arr.put(inputValues1[1], inputValues1[0]);
-//				cd.sety(inputValues1[1]);
-//				arr.add(cd);
 			}
 			con=new Connections();
 			tokn=new TokenObtain();
-			
 			token=con.connection_establish("girvanasah", "DW2ivRcDTaqHC9PoYuR1Xj3fXdpYCRS11NY2PAg8axI=",
 					"http://api.microsofttranslator.com", "client_credentials");
-		
 			finaltoken=tokn.getToken(token);
 			int j=0;
-			while ((line = br.readLine()) != null) {
-              
+			while ((line = br.readLine()) != null)
+			{
 				inputValues = line.split(cvsSplitBy);
-				 from=inputValues[0];
-				 to=inputValues[1];
-				 text = inputValues[2];
-				 if(from.isEmpty()){
+				from=inputValues[0];
+				to=inputValues[1];
+				text = inputValues[2];
+				if(from.isEmpty())
+				{
 					 from=Translate.lang_from_DropDown.get(j);
-				 }
-				 if(arr.containsKey(from)){
-					 fromCode=arr.get(from);
-				 }
-				 if(arr.containsKey(to)){	
+				}
+				if(arr.containsKey(from))
+				{
+					fromCode=arr.get(from);
+				}
+				if(arr.containsKey(to))
+				{	
 					 toCode=arr.get(to);
-				 }
-								
+				}
 				getAuthentication( fromCode , toCode , text,finaltoken);
 				j++;
 		
 			}	
 		}
 		catch (Exception e){}
-	return	resultFromAPI;
+		return	resultFromAPI;
 	}
 	public void getAuthentication(String from , String to , String text, String finaltoken) throws UnsupportedEncodingException 
 	{
-		
 		XMLParsing xml;
-	System.out.println(from+" "+to);
-			String url = "http://api.microsofttranslator.com/V2/Http.svc/Translate?text="+  URLEncoder.encode(text,"UTF-8") + "&from=" + from + "&to=" + to;
-			
-			HttpClient client = HttpClientBuilder.create().build();
-			
-			HttpGet request = new HttpGet(url);
-			
-			try 
-			{
+		System.out.println(from+" "+to);
+		String url = "http://api.microsofttranslator.com/V2/Http.svc/Translate?text="+  URLEncoder.encode(text,"UTF-8") + "&from=" + from + "&to=" + to;
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(url);
+		try 
+		{
 			// add request header
-				
-				finaltoken = "Bearer " + finaltoken;
-				request.addHeader("Authorization", finaltoken);
-//				System.out.println("heyaaa"+finaltoken);
-				HttpResponse response = client.execute(request);
-
-//				System.out.println("\nSending 'GET' request to URL : " + url);
-//				System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
-
-				BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-
-				StringBuffer result = new StringBuffer();
-				String line = "";
-				while ((line = rd.readLine()) != null) 
-				{
-					result.append(line);
-				}
-				 xml=new XMLParsing();
-//				result1 = result.toString();
-				System.out.println(result);
-				String resultBing=xml.parseXML(result);
-				resultFromAPI.add(resultBing);
-			}
-			catch (Exception e) 
+			finaltoken = "Bearer " + finaltoken;
+			request.addHeader("Authorization", finaltoken);
+			HttpResponse response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) 
 			{
-				e.printStackTrace();
+				result.append(line);
 			}
+			xml=new XMLParsing();
+			System.out.println(result);
+			String resultBing=xml.parseXML(result);
+			resultFromAPI.add(resultBing);
 		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
-
